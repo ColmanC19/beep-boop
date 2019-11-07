95e9349c6a9756359b1b8eedc15b2262729170cd
Colman Currie <curriecolman@gmail.com>
2019-11-06 21:17:45 -0800

    reorganize business/user logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,24 @@
 
+//  user logic
+
+var aResult = function(number) {
+  var result = [];
+  for (i = 0; i <= number; i++){
+    var iString = i.toString();
+    var name = $("#text").val();
+    if (iString.includes("3")) {
+      result.push("I'm sorry " + name + ", I'm afraid I can't do that!");
+    } else if (iString.includes("2")) {
+      result.push("Boop!");
+    } else if (iString.includes("1")) {
+      result.push("Beep!");
+    } else {
+      result.push(" " + i);
+    }
+  }
+  return result;
+}
+
 // business logic
 
 $(document).ready(function() {
@@ -12,23 +32,3 @@ $(document).ready(function() {
 
     });
 });
-
-//  user logic
-
-var aResult = function(number) {
-  var result = [];
-  for (i = 0; i <= number; i++){
-    var iString = i.toString();
-    var name = $("#text").val();
-    if (iString.includes("3")) {
-      result.push("I'm sorry " + name + ","+ " I'm afraid I can't do that!");
-    } else if (iString.includes("2")) {
-      result.push("Boop!");
-    } else if (iString.includes("1")) {
-      result.push("Beep!");
-    } else {
-      result.push(" " + i);
-    }
-  }
-  return result;
-}
