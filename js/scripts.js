b43dbcb52d7eba0138867a14508b0f9b3bdeb065
Colman Currie <curriecolman@gmail.com>
2019-11-10 20:14:41 -0800

    add name validation

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
 
 //  user logic
 
-var aResult = function(number) {
+var aResult = function(number, name) {
   var result = [];
   for (i = 0; i <= number; i++){
     var iString = i.toString();
-    var name = $("#text").val();
     var nameU = name.toUpperCase();
     if (iString.includes("3")) {
       result.push("I'm sorry " + nameU + ", I'm afraid I can't do that!");
@@ -24,12 +23,15 @@ $(document).ready(function() {
     $("form#BeepBoop").submit(function(event) {
         event.preventDefault();
         $("#text").val();
+        var name = $("#text").val();
+        if (name === "") {
+          alert("Please enter a name, john Doe");
+        }
         var number = $("#number").val();
-        console.log(number);
         if (number === "") {
           alert("Please enter a number greater than 0")
         }
-        var result = aResult(number);
+        var result = aResult(number, name);
         $("#text").val(" ");
         $("#number").val(" ");
         $("#output").text(result);
